a509ec87482eaf58944e7caabc1c1cb3b7547f4a
divhya <divhya.bharathy@kanini.com>
2020-02-06 18:53:05 +0530

    change 1 done
    correct the program.cs file i will mail that code in mail u transfer to webapi and do d changes

---
+++ SimpleCalculator/SimpleArithmeticCalculator/Commands/AddCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleArithmeticCalculator.Commands
+{
+    class AddCommand
+    {
+    }
+}
---
+++ SimpleCalculator/SimpleArithmeticCalculator/Commands/SubtractionCommand.cs
@@ -0,0 +1,15 @@
+﻿using SimpleArithmeticCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleArithmeticCalculator.Commands
+{
+   public class SubtractionCommand : ICalculatorCommand
+   {
+        public double Calculate(double firstValue,double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+   }
+}
