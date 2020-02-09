846600732d23ddd5fd17b10aba2e23fb824dc2ad
Mrahamed <57354059+ahamed1997@users.noreply.github.com>
2020-02-09 16:30:25 +0530

    WebAPI Connectivity Completed
    Cross Origin Problem resolved.

--- SimpleCalculator/SimpleCalculator/Startup.cs
+++ SimpleCalculator/SimpleCalculator/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using System.Web.Http.Cors;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SimpleCalculator
 {
@@ -20,12 +15,17 @@ namespace SimpleCalculator
         {
             Configuration = configuration;
         }
-
+       
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowMyOrigin",
+                builder => builder.WithOrigins("http://localhost:4200"));
+            });
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -51,11 +51,11 @@ namespace SimpleCalculator
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+           
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-            app.UseCors();
+            app.UseCors("AllowMyOrigin");
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
