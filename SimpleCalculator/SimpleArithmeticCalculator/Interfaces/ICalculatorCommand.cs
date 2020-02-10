5f85d0588bf0a63f2f1ec813d5077152355fe662
divhya <divhya.bharathy@kanini.com>
2020-02-10 15:04:05 +0530

    Stylecop added

--- SimpleCalculator/SimpleArithmeticCalculator/Interfaces/ICalculatorCommand.cs
+++ SimpleCalculator/SimpleArithmeticCalculator/Interfaces/ICalculatorCommand.cs
@@ -1,7 +1,19 @@
-﻿namespace SimpleArithmeticCalculator.Interfaces
+﻿// <copyright file="ICalculatorCommand.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SimpleArithmeticCalculator.Interfaces
 {
+    /// <summary>
+/// ICalculatorCommand interface created.
+/// </summary>
     public interface ICalculatorCommand
-    {
+    {/// <summary>
+    /// Calculate method to compute values.
+    /// </summary>
+    /// <param name="firstValue">Inputs first value.</param>
+    /// <param name="secondValue">Inputs Secon value.</param>
+    /// <returns>Returns the result.</returns>
         double Calculate(double firstValue, double secondValue);
     }
 }
--- SimpleCalculator/SimpleArithmeticCalculator/Interfaces/ICalculatorCreate.cs
+++ SimpleCalculator/SimpleArithmeticCalculator/Interfaces/ICalculatorCreate.cs
@@ -1,10 +1,21 @@
-﻿using SimpleArithmeticCalculator.Enums;
-
+﻿// <copyright file="ICalculatorCreate.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace SimpleArithmeticCalculator.Interfaces
 {
-    public interface ICalculatorCreate 
+    using SimpleArithmeticCalculator.Enums;
+
+    /// <summary>
+    /// Interface to input command type from user.
+    /// </summary>
+    public interface ICalculatorCreate
     {
+        /// <summary>
+        /// Method Create Command which inputs command type from user.
+        /// </summary>
+        /// <param name="commandType">Inputs Command type.</param>
+        /// <returns>Selects the command and perform calculation.</returns>
          ICalculatorCommand CreateCommand(CommandType commandType);
     }
 }
