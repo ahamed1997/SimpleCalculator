1f5b9276454803b35e9a86d9d2965edff7f1e447
Mrahamed <57354059+ahamed1997@users.noreply.github.com>
2020-02-10 20:39:40 +0530

    Updation in Naming Convertion.
    Updation in Naming Convertion.

--- SimpleCalculator/SimpleCalculator/Controllers/MathCalculatorController.cs
+++ SimpleCalculator/SimpleCalculator/Controllers/MathCalculatorController.cs
@@ -30,9 +30,9 @@ namespace SimpleCalculator.Controllers
         public double ArithmeticCalculator(double firstValue = 0, double secondValue = 0, int operationType = 1)
         {
             ICalculateCommandFactory calculateCommandFactory;
-            
+            Calculator calculator = new Calculator();
             var commandType = (CommandType)operationType;
-            double output = calculateCommandFactory.CreateCommand(commandType).Calculate(firstValue, secondValue);
+            double output = calculator.CreateCommand(commandType).Calculate(firstValue, secondValue);
             return output;
         }
     }
