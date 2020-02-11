b17df49c9141d29cd534a7312abc83f3aa499ca2
divhya <divhya.bharathy@kanini.com>
2020-02-11 10:59:32 +0530

    tgdfhr

--- SimpleCalculator/SimpleArithmeticCalculator/Commands/DivideCommand.cs
+++ SimpleCalculator/SimpleArithmeticCalculator/Commands/DivideCommand.cs
@@ -1,12 +1,22 @@
-﻿using SimpleArithmeticCalculator.Interfaces;
+﻿// <copyright file="DivideCommand.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace SimpleArithmeticCalculator.Commands
 {
+    using SimpleArithmeticCalculator.Interfaces;
+
+    /// <summary>
+    /// DivideCommand implements ICalculatorCommand.
+    /// </summary>
     public class DivideCommand : ICalculatorCommand
     {
-        public double Calculate(double firstValue,double secondValue)
-        {
-            return firstValue / secondValue;
-        }
+        /// <summary>
+        /// Method Calculate.
+        /// </summary>
+        /// <param name="firstValue">Inputs first value.</param>
+        /// <param name="secondValue">Inputs second value.</param>
+        /// <returns>Return addition result.</returns>
+        public double Calculate(double firstValue, double secondValue) => firstValue / secondValue;
     }
 }
--- SimpleCalculator/SimpleArithmeticCalculator/Enums/CommandType.cs
+++ SimpleCalculator/SimpleArithmeticCalculator/Enums/CommandType.cs
@@ -1,12 +1,30 @@
-﻿namespace SimpleArithmeticCalculator.Enums
-{
+﻿// <copyright file="CommandType.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SimpleArithmeticCalculator.Enums
+{/// <summary>
+/// Enum is created to choose the command type.
+/// </summary>
     public enum CommandType
-    {
-
+    {/// <summary>
+    /// Add Command.
+    /// </summary>
         Add = 1,
-        Subtract = 2,
-        Multiply = 3,
-        Divide = 4
 
+        /// <summary>
+        /// Subtract Command.
+        /// </summary>
+        Subtract = 2,
+
+        /// <summary>
+        /// Multiply Command.
+        /// </summary>
+        Multiply = 3,
+
+        /// <summary>
+        /// Divide Command.
+        /// </summary>
+        Divide = 4,
     }
 }
