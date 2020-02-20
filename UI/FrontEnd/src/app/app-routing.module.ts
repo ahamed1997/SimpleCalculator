c76162991494487afcebf1ca7254769f1d7d9a4d
Mrahamed <57354059+ahamed1997@users.noreply.github.com>
2020-02-20 17:41:24 +0530

    Angular Refactor Completed.
    Angular Refactor Completed.

--- UI/FrontEnd/src/app/app-routing.module.ts
+++ UI/FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Route, RouterModule,CanActivate } from '@angular/router';
+import { Route, RouterModule} from '@angular/router';
 import { StandardCalculatorComponent } from './components/standard-calculator/standard-calculator.component';
 import { BasicCalculatorComponent } from './components/basic-calculator/basic-calculator.component';
 
-
 const myRoutes:Route[]=[
-  { path: '', redirectTo: 'standardCalculator', pathMatch: 'full' },
+  {path: '', redirectTo: 'standardCalculator', pathMatch: 'full' },
   {path:'basicCalculator',component:BasicCalculatorComponent},
   {path:'standardCalculator',component:StandardCalculatorComponent}]
 
--- UI/FrontEnd/src/app/services/calculator.service.ts
+++ UI/FrontEnd/src/app/services/calculator.service.ts
@@ -1,23 +1,68 @@
+/**
+* Imports Reqirements.
+*/
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
+/**
+* Dependency Injection.
+*/
 @Injectable({
     providedIn:'root'
 })
 
+/**
+* Export CalculatorService.
+*/
 export class CalculatorService
 {
-    result:any;
+result:any;
+readonly rootURL="https://localhost:44333/MathCalculator/ArithmeticCalculator?firstValue="
+
+/**
+* Creates an instance of CalculatorService.
+*/
 constructor(private myHttp:HttpClient)
 {
     this.result;
 }
-calculation(firstValue:any,secondValue:any,operation:any):Observable<any>
+
+/**
+* // TODO: Arithmetic Operation.
+* API call to Back End.
+* @param Operand1.
+* @param Operand2.
+* @param operationType.
+* @returns calculated answer.
+*/
+calculate(firstValue:any,secondValue:any,operation:any):Observable<any>
 {
-this.result = this.myHttp.get("https://localhost:44333/MathCalculator/ArithmeticCalculator?firstValue="+firstValue+"&secondValue="+secondValue+"&operationType="+operation);
-console.log('result',this.result)
-return this.result;
+operation = this.getOperator(operation);
+return this.myHttp.get(this.rootURL+firstValue+"&secondValue="+secondValue+"&operationType="+operation);
+}
+
+/**
+* // TODO: Finding Operation.
+* Find the operations.
+* @param operator.
+* @returns Operation type.
+*/
+getOperator(operator):number
+{
+    if( operator=="+")
+    {
+        return 1;
+    }
+    else if ( operator =="-")
+    {
+        return 2;        }
+    else if (operator == "*")
+    {
+        return 3;        }
+    else if (operator == "/")
+    {
+        return 4;        
+    }
 }
 }
